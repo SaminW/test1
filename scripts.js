cb74bb99c53facaa31700619790ed4550bfc52a3
SaminW <samin931208@hotmail.com>
2017-12-17 14:12:57 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,67 @@
+/*console.log("Hi!");
+console.log(3==4||4=="4")
+console.log(32!=4&&4<32)
+console.log(!(32===32))
+var myNumber=3;
+var fruit="apples"
+console.log("we have " + (myNumber*3) + " " + fruit);
+var score=prompt("What's your score")
+if (score>=50) {
+	console.log("You passed!");
+} else{
+	console.log("Keep trying...");
+}
+var food=prompt("你最爱吃的食物是?")
+if (food=="酸辣粉") {
+	alert("good taste!");
+} else if (food=="鸭头") {
+	alert("饿了？吃啊！")
+}else {
+	console.log("yuck");
+}*/
+
+
+/*var fruit="apples";
+for (var number = 7;number<=20; number=number+3) {
+	console.log("we have " + number + " " + fruit);
+};
+
+for (var number = 7;number<=20; number=number+4) {
+	console.log("we have " + number + " " + fruit);
+};
+
+function add(a,b) {
+	var c = a +b;
+	return c;
+}
+
+var x=function (a,b) {return a*b};
+var z=x(4,3);
+
+console.log(z)*/
+
+/*$(function(){
+	$("h2").append(" when to?");
+	$("a").css("font-size","40");
+	$(".hello p").css("background-color","#ccc");
+	$(".pic").click(function(){
+		$("h1").text("new text");
+	});
+	$(".pic").click(function(){
+		$("h1").fadeOut()
+	});
+
+});*/
+
+$(function(){
+	$("")
+})
+
+/*var myNumber=3;
+
+for (var "we have " + (myNumber) + " " + fruit = 0; "we have " + (myNumber*3) + " " + fruit=3; < Things.length; i++) {
+	Things[i]
+}
+console.log("we have " + (myNumber) + " " + fruit)*/
+
+
